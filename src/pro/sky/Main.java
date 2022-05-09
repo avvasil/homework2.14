ac91b6fbf1e27124f515961eb44b71aafe7a1e39
avvasil <avvasil@mail.ru>
2022-05-09 21:54:38 +0300

    homework2.14

---
+++ src/pro/sky/Main.java
@@ -0,0 +1,63 @@
+package pro.sky;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyArrayList test = new MyArrayList();
+        MyArrayList otherList = new MyArrayList();
+
+        test.add("1");
+        test.add("2");
+        test.add("3");
+        test.add("4");
+        test.add("5");
+        test.add("6");
+        test.add("7");
+        test.add("8");
+        test.add("9");
+        System.out.println(test);
+
+//        otherList.add("1");
+//        otherList.add("2");
+//        otherList.add("3");
+//        otherList.add("4");
+//        otherList.add("5");
+//        otherList.add("6");
+//        otherList.add("7");
+//        otherList.add("8");
+//        otherList.add("9");
+
+//        test.add(4, "ADD_NEW");
+//        System.out.println(test);
+
+//        test.set(5, "SET_NEW");
+//        System.out.println(test);
+
+//        test.remove("5");
+//        System.out.println(test);
+
+//        test.remove(3);
+//        System.out.println(test);
+
+//        System.out.println(test.contains("5"));
+
+//        System.out.println(test.indexOf("7"));
+
+//        System.out.println(test.lastIndexOf("4"));
+
+//        System.out.println(test.get(78));
+
+//        System.out.println(test.equals(otherList));
+//        System.out.println("OtherList  " + Arrays.toString(otherList.toArray()));
+
+//        System.out.println(test.size);
+
+//        System.out.println(test.isEmpty());
+
+//        test.clear();
+//        System.out.println(test);
+
+//        System.out.println("New Array  " + Arrays.toString(test.toArray()));
+
+    }
+}
